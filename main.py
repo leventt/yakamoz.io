1e755f399a880848cb45d97f3dd19ca25c6abdf9
leventt <leventtasel@gmail.com>
2020-03-15 19:40:46 -0700

    pushing stuff until things work
    as always

--- main.py
+++ main.py
@@ -102,4 +102,24 @@ def maskNeutral():
     return json.dumps(neutral)
 
 
+@route('/')
+def index():
+    return static_file('index.html', os.path.join(ROOT, 'static'))
+
+
+@route('/<staticFile>')
+def staticStuff(staticFile):
+    return static_file(staticFile, os.path.join(ROOT, 'static'))
+
+
+@route('/external/js/<staticFile>')
+def externalJS(staticFile):
+    return static_file(staticFile, os.path.join(ROOT, 'external/js/'))
+
+
+@route('/external/fonts/<staticFile>')
+def externalFonts(staticFile):
+    return static_file(staticFile, os.path.join(ROOT, 'external/fonts/'))
+
+
 app = bottle.default_app()
